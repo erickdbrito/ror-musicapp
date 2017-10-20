3ea8c3712ad4eb4d1fe59cb61e2392f512be3c3a
Erick Brito <erick.dbrito@gmail.com>
2017-10-20 16:31:30 -0500

    Manual creation Model, views, controller, migration and route Playlist

---
+++ app/controllers/playlists_controller.rb
@@ -0,0 +1,57 @@
+class PlaylistsController < ApplicationController
+  def index
+     @playlists = Playlist.all
+  end
+
+  def show
+    @playlist = Playlist.find(params[:id])
+  end
+
+  def edit
+    @playlist = Playlist.find(params[:id])
+  end
+
+  def new
+    @playlist = Playlist.new
+  end
+
+  # POST /playlists
+  def create
+    @playlist = Playlist.new(playlist_params)
+
+    respond_to do |format|
+      if @playlist.save
+        format.html { redirect_to @playlist, notice: 'Playlist was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  # PATCH/PUT /playlists/1
+  def update
+    @playlist = Playlist.find(params[:id])
+
+    respond_to do |format|
+      if @playlist.update(playlist_params)
+        format.html { redirect_to @playlist, notice: 'Playlist was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  # DELETE /playlists/1
+  # DELETE /playlists/1.json
+  def destroy
+    @playlist.destroy
+    respond_to do |format|
+      format.html { redirect_to playlists_url, notice: 'Playlist was successfully destroyed.' }
+    end
+  end
+
+  def playlist_params
+    params.require(:playlist).permit(:name, :likes, :description)
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171020184230) do
+ActiveRecord::Schema.define(version: 20171020205121) do
 
-  create_table "tracks", force: :cascade do |t|
+  create_table "playlists", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "likes"
+  end
+
+  create_table "tracks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
     t.string "album"
     t.string "artist"
